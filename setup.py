7c828c20862e34af58c1d0bb7d8cb4992913be06
William Bushey <wbushey@gmail.com>
2014-03-27 18:01:32 -0500

    Bumped version and development status Swapped gs.skin.ogn.edem for edem.skin

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='edem.group.type.discussion',
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers for values 
     classifiers=[
-      "Development Status :: 1 - Planning",
+      "Development Status :: 5 - Production/Stable",
       "Environment :: Web Environment",
       "Framework :: Zope2",
       "Intended Audience :: Developers",
@@ -33,7 +33,7 @@ setup(name='edem.group.type.discussion',
     zip_safe=False,
     install_requires=[
         'setuptools',
-        'gs.skin.ogn.edem',
+        'edem.skin',
         'gs.group.type.discussion',
         'gs.group.member.canpost',
         # -*- Extra requirements: -*-
